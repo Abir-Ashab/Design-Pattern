c98a2a4ab227cd385613b7d164d936a122b4512e
Abir Ashab Niloy <bsse1315@iit.du.ac.bd>
2024-05-20 00:02:01 +0600

    Add files via upload

---
+++ Proxy/src/proxy/Main.java
@@ -0,0 +1,38 @@
+/*
+ The library provides us with the video downloading class. However, it’s very inefficient. 
+ If the client application requests the same video multiple times, 
+ the library just downloads it over and over, 
+ instead of caching and reusing the first downloaded file.
+ The proxy class implements the same interface as the original downloader and delegates it 
+ all the work. However, it keeps track of the downloaded files and returns the cached result
+ when the app requests the same video multiple times.
+*/
+package proxy;
+
+public class Main {
+	public static void main(String[] args) {
+        YouTubeDownloader naiveDownloader = new YouTubeDownloader(new ThirdPartyYouTubeClass());
+        YouTubeDownloader smartDownloader = new YouTubeDownloader(new YouTubeCacheProxy());
+
+        long naive = test(naiveDownloader);
+        long smart = test(smartDownloader);
+        System.out.print("Time saved by caching proxy: " + (naive - smart) + "ms");
+
+    }
+    private static long test(YouTubeDownloader downloader) {
+        long startTime = System.currentTimeMillis();
+
+        // User behavior in our app:
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("catzzzzzzzzz");
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("dancesvideoo");
+        // Users might visit the same page quite often.
+        downloader.renderVideoPage("catzzzzzzzzz");
+        downloader.renderVideoPage("someothervid");
+
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.print("Time elapsed: " + estimatedTime + "ms\n");
+        return estimatedTime;
+    }
+}
